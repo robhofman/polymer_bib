c888211610045e0d930fcd1e9a23ef77711825b9
robhofman <rob.hofman@student.howest.be>
2017-02-12 12:50:36 +0100

    calendar-lite-3 added

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -18,21 +18,7 @@ if(document.documentElement.clientWidth>1200){
 
 
 
-window.addEventListener('WebComponentsReady', function(e) {
-    //todo de data niet met time out inladen.. dit is niet proper maar zonder is dates.length steeds 0
-    setTimeout(function(){
-        var dates = document.getElementsByClassName("dateSticker");
-        console.log(dates.length);
-        for(var i=0; i<dates.length; i++){
-            if(i==13){
-                //ter voorbeeld dat we de elementen van hieruit kunnen benaderen. Als we er op klikken verandert de BG wel niet meer.
-                dates[i].style.background = "red";
-                dates[i].style.color = "white";
-            }
-        }
-    }, 10);
 
-});
 
 
 document.querySelector('#cal').addEventListener('date-change', function (e) {
